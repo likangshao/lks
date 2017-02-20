ad8398d109528ec5172006c27c09d170aa80b7ac
lkss <lks.com>
2017-02-20 20:04:25 +0800

    xxx

---
+++ kuangjia.js
@@ -0,0 +1,28 @@
+/**
+ * Created by likangshao on 2017/2/19.
+ */
+
+$({
+    each: function( obj, callback ) {
+        var length, i = 0;
+
+        if ( isArrayLike( obj ) ) {
+            length = obj.length;
+            for ( ; i < length; i++ ) {
+                if ( callback.call( obj[ i ], i, obj[ i ] ) === false ) {
+                    break;
+                }
+            }
+        } else {
+            for ( i in obj ) {
+                if ( callback.call( obj[ i ], i, obj[ i ] ) === false ) {
+                    break;
+                }
+            }
+        }
+        return obj;
+    },
+    extend:function(){
+
+    }
+})
